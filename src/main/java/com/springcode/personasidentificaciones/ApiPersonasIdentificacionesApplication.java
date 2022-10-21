e8b80b628336fed069ec6307f871474b25eab54a
Jheyson Garcia Guerra <jheyson.garcia2010@gmail.com>
2022-10-20 21:11:07 -0500

    Creación de proyecto spring personas-identificaciones

---
+++ src/main/java/com/springcode/personasidentificaciones/ApiPersonasIdentificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.springcode.personasidentificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonasIdentificacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPersonasIdentificacionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcode/personasidentificaciones/ApiPersonasIdentificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcode.personasidentificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonasIdentificacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
